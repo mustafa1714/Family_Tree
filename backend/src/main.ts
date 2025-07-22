fdbbcb4da3d0ab6e1cfe9d10f3080f98f4e9014f
mustafa1714 <mustafa1714.dev@gmail.com>
2025-07-22 05:02:09 +0300

    Initiate App

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // prisma
+  const prismaService = app.get('PrismaService');
+  await prismaService.enableShutdownHooks(app);
+
+  // cors
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
